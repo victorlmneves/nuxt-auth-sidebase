04339cc289d52becc3d74cd4d6000ae568222c1c
Victor Neves <info@victorneves.dev>
2024-07-10 20:45:48 +0100

    Update [...].ts

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,50 +1,33 @@
-import CredentialsProvider from 'next-auth/providers/credentials'
-import GithubProvider from 'next-auth/providers/github'
+import Auth0Provider from 'next-auth/providers/auth0'
 import { NuxtAuthHandler } from '#auth'
 
+const {
+  AUTH0_ENCRYPTION_SECRET,
+  AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET,
+  AUTH0_ISSUER_BASE_URL,
+  AUTH0_MEK_AUDIENCE,
+  public: { isDeployed }
+} = useRuntimeConfig()
+
+const AUTH0_SCOPE =
+  'openid profile email offline_access read:bsr:cmdm read:bsr:pc read:bsr:thunderhead read:bsr:permission read:bsr:pin'
+const AUTH0_CHECKS = ['state']
+const AUTH0_RESPONSE_TYPE = 'code'
+
 export default NuxtAuthHandler({
-  // TODO: SET A STRONG SECRET, SEE https://sidebase.io/nuxt-auth/configuration/nuxt-auth-handler#secret
-  secret: process.env.AUTH_SECRET || 'my-auth-secret',
-  // TODO: ADD YOUR OWN AUTHENTICATION PROVIDER HERE, READ THE DOCS FOR MORE: https://sidebase.io/nuxt-auth
+  debug: !isDeployed,
+  secret: AUTH0_ENCRYPTION_SECRET,
   providers: [
-    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-    GithubProvider.default({
-      clientId: process.env.GITHUB_CLIENT_ID || 'enter-your-client-id-here',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || 'enter-your-client-secret-here'
-    }),
-    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-    CredentialsProvider.default({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: '(hint: jsmith)' },
-        password: { label: 'Password', type: 'password', placeholder: '(hint: hunter2)' }
-      },
-      authorize (credentials: any) {
-        console.warn('ATTENTION: You should replace this with your real providers or credential provider logic! The current setup is not safe')
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // NOTE: THE BELOW LOGIC IS NOT SAFE OR PROPER FOR AUTHENTICATION!
-
-        const user = { id: '1', name: 'J Smith', username: 'jsmith', password: 'hunter2' }
-
-        if (credentials?.username === user.username && credentials?.password === user.password) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          console.error('Warning: Malicious login attempt registered, bad credentials provided')
-
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
+    Auth0Provider.default({
+      id: 'auth0',
+      clientSecret: AUTH0_CLIENT_SECRET,
+      clientId: AUTH0_CLIENT_ID,
+      issuer: AUTH0_ISSUER_BASE_URL,
+      responseType: AUTH0_RESPONSE_TYPE,
+      checks: AUTH0_CHECKS,
+      scope: AUTH0_SCOPE,
+      authorization: { params: { scope: SCOPE }, audience: AUTH0_MEK_AUDIENCE }
     })
   ]
 })
